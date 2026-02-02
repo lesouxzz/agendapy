904faf4dfcb4ebcb60ed4231805c53de0cf218dd
lesouxzz <leticia.a.souza11@aluno.senai.br>
2026-02-02 16:49:40 -0300

    feat: salvando as tarefas

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from models.tarefa import Tarefa
 
 app = Flask(__name__)
- 
+
 @app.route('/')
 def home():
-    return '<p style=c"color:red">Seja bem-vindo'
+    return render_template('home.html', titulo='Home')
 
- @app.route('/ola')
+@app.route('/agenda', methods=['GET', 'POST'])
+def agenda():
+    tarefa = None
+    
+    if request.method == 'POST':
+        titulo_tarefa = request.form['titulo-tarefa']
+        data_conclusao = request.form['data-conclusao']
+        tarefa = Tarefa(titulo_tarefa, data_conclusao)
+
+    return render_template('agenda.html', titulo='Agenda', tarefa=tarefa)
+
+@app.route('/ola')
 def ola_mundo():
     return "Olá, Mundo!"
